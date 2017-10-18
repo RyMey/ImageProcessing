17da5ba2a805fc2df5b2234a03acca8d639aa9eb
Rya Meyvriska <rya.meyvriska@mail.ugm.ac.id>
2017-10-19 00:43:05 +0700

    add function contrast

---
+++ 1pointProcessing/header/contrast.h
@@ -0,0 +1,33 @@
+Mat contrast(Mat source){
+    Mat result;
+    result = source;
+    int width = source.size().width;
+    int height = source.size().height;
+    int minContrast = 255;
+    int maxContrast = 0;
+
+    for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
+            Vec3b temp = source.at<Vec3b>(i,j);
+            for(int k=0;k<3;k++){
+                int value = (int) source.at<Vec3b>(i,j).val[k];
+                if(value<minContrast)
+                    minContrast = value;
+                else if(value>maxContrast)
+                    maxContrast = value;
+            }
+        }
+    }
+
+    for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
+            Vec3b temp = source.at<Vec3b>(i,j);
+            for(int k=0;k<3;k++){
+                int value = (int) source.at<Vec3b>(i,j).val[k];
+                result.at<Vec3b>(i,j).val[k] = (value-minContrast)*(255/(maxContrast-minContrast));
+            }
+        }
+    }
+
+    return result;
+}
