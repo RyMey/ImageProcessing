7eacbe7eb8c9056c22af7218bd9f7135591cd1f4
Rya Meyvriska <rya.meyvriska@mail.ugm.ac.id>
2017-10-19 13:14:31 +0700

    Add more method from crop, now they are 2 method (for shilouette, and substract), and add method getObject if main want to do something like print

--- 1pointProcessing/header/crop.h
+++ 1pointProcessing/header/crop.h
@@ -1,4 +1,4 @@
-Mat crop(Mat source){
+Mat cropSilhouette(Mat source){
     Mat result,object;
     object = imread("picture/rya.jpg",0);
 
@@ -15,4 +15,36 @@ Mat crop(Mat source){
     }
 
     return result;
+}
+
+Mat cropSubtract(Mat source){
+    Mat result,object;
+    object = imread("picture/rya.jpg",0);
+
+    result = source;
+    int width = source.size().width;
+    int height = source.size().height;
+
+    for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
+            Vec3b value = object.at<uchar>(i,j);
+            for(int k=0;k<3;k++){
+                int temp = result.at<Vec3b>(i, j).val[k]-value.val[k];
+                if(temp>255)
+                    result.at<Vec3b>(i, j).val[k]=255;
+                else if(temp<0)
+                    result.at<Vec3b>(i, j).val[k]=0;
+                else
+                    result.at<Vec3b>(i, j).val[k] = temp;
+            }
+        }
+    }
+
+    return result;
+}
+
+Mat getObject(){
+    Mat object;
+    object = imread("picture/rya.jpg",0);
+    return object;
 }
